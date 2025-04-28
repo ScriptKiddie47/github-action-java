d63eadc88bcc7a632caa724d5fe668f7119664c4
ritam bala <ritambala@live.com>
2025-04-28 17:59:52 +0530

    first-commit

---
+++ src/main/java/com/syndicate/github/action/GithubActionJavaApplication.java
@@ -0,0 +1,13 @@
+package com.syndicate.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syndicate/github/action/GithubActionJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syndicate.github.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
